08c2cff837a61a42587705a0abb6bf397a85fda8
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-08 20:18:53 +0500

    added mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace deploy_ci_cd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
